ee4d84b65bf22832e3619c2fd7457cd85e184f12
Suporno <suporno.chaudhury@gmail.com>
2018-05-19 20:53:04 +0530

    New activities
    Very minor update with only the new activity being made.

--- app/src/main/java/com/example/shantanu/opus/MainActivity.java
+++ app/src/main/java/com/example/shantanu/opus/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.shantanu.opus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void companyLogin(View view){
+
+        Intent intent = new Intent(this, companyLoginActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void individualLogin(View view){
+
+        Intent intent = new Intent(this, individualLoginActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void adminLogin(View view){
+
+        Intent intent = new Intent(this, sysAdminLoginActivity.class);
+        startActivity(intent);
+
     }
 }
